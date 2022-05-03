2b7f5574d8afc36674dbbd66d581c5aa1c1f740c
Aszalea-Calderon <aszalea_calderon@yahoo.com>
2022-05-03 08:41:13 -0400

    excuting file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.Read();
+// using System;
+
+// namespace HelloWorld
+// {
+//     class Program
+//     {
+//         //Entry Point(
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello, World!");
+//             Console.WriteLine("Hello, Asz!");
+
+//         }
+//     }
+// }
