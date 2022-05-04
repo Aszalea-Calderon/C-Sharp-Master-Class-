4db5df686c3f0ee19b6458cd866486203316b6eb
Aszalea-Calderon <aszalea_calderon@yahoo.com>
2022-05-04 08:28:33 -0400

    Variables converstaion

---
+++ Variables.cs
@@ -0,0 +1,25 @@
+//Variables must have a Type, Name, and Data
+
+//INT (Whole Numbers)
+// For example if we are storing an int (a whole number) we would write it like the following
+//-- int iAmANumber = 5; --//
+// This example stores the type of data (Int) the name of the variable, and the number itself. 
+
+
+//FLOATs (Decimals)
+// Floats are used to store numbers with additional numbers after a decimal.
+// as we know, writing functions in C# asks for a type, name and data type. So we could write this like:
+//-- float pi = 3.1415 --//
+
+//BOOL (True false)
+// Bools are true false statements
+// an example of how this is written is as follows
+//-- bool isGPSEnabled = true; --//
+
+//STRING (Written words) 
+//String is written words, Note that strings take more memory than a char 
+//-- string myName = "Aszalea"
+
+//Char (characters)
+//This is used to store special characters, use this if you only need to store one letter
+//-- char at = "@"
